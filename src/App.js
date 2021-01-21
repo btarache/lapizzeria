7dff297560607714ab2cdd5bd1c4736caee8a162
btarache <benjamintarache@gmail.com>
2021-01-21 10:23:01 -0600

    Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import About from "./components/About";
+import Shop from './components/Shop';
+import Menu from './components/Menu';
+import Clients from "./components/Clients";
+import Prices from './components/Prices';
 
 function App() {
   return (
-    <div>
-      <h1>Aqui mi pag web</h1>
-    </div>
+    <>
+      <Header />
+      <About />
+      <Shop />
+      <Menu />
+      <Clients />
+      <Prices />
+    </>
   );
 }
 
